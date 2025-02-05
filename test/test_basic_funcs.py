f3281a75dbb38bde3813d22554d3a8ed5c3ffa78
Kwan Yuet Stephen Ho <3810067+stephenhky@users.noreply.github.com>
2025-02-05 13:40:12 -0500

    updated unit test

--- test/test_basic_funcs.py
+++ test/test_basic_funcs.py
@@ -11,7 +11,7 @@ class TestBasic(unittest.TestCase):
     def test_tukeyh(self):
         np.testing.assert_almost_equal(
             tutils.tukeyh(np.array([-100., 0., 1]), 1.5),
-            np.array([0., 0., exp(0.5*1.5)])
+            np.array([-np.inf, 0., exp(0.5*1.5)])
         )
         np.testing.assert_almost_equal(
             tutils.tukeyh(np.array([-100., 0., 11.2, 25.4]), 0.),
@@ -19,7 +19,7 @@ class TestBasic(unittest.TestCase):
         )
         np.testing.assert_almost_equal(
             tutils.tukeyh(np.array([-110., 0., 34.2]), -10.),
-            np.array([-np.inf, 0., 0.])
+            np.array([0., 0., 0.])
         )
 
 
